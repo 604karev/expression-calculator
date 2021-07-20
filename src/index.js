b1ea06f2d99d3d221bc916052a767b7a20ef6e04
604karev <604karev@gmail.com>
2021-07-20 21:39:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function expressionCalculator(str) {
+    const symbolsEntryCount = str => sybmol => (str.toString().split(sybmol).length - 1);
+    const symbolsCount = symbolsEntryCount(str);
+    const cutArray = str.split('').filter(element => element !== ' ');
+    if (symbolsCount(')') === symbolsCount('(')) {
+        for (let i = 0; i < cutArray.length; i++) {
+            if (cutArray[i] === '0' && cutArray[i - 1] === '/') {
+                throw ('TypeError: Division by zero.')
+            }
+        }
+        return Function(`return ${str}`)();
+    } else {
+        throw ('ExpressionError: Brackets must be paired');
+    }
 }
 
 module.exports = {
